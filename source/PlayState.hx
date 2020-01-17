2f0c71e4e06c63c509fd49315994f3ba622e68ac
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2020-01-17 13:59:20 -0800

    init commits

---
+++ source/PlayState.hx
@@ -0,0 +1,16 @@
+package;
+
+import flixel.FlxState;
+
+class PlayState extends FlxState
+{
+	override public function create():Void
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+	}
+}
