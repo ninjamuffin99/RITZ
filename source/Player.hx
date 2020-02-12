61c7bf86e0de26fb0becd70d6f7d1fc0a4113d29
George FunBook <gkurelic@gmail.com>
2020-02-12 17:45:02 -0600

    simple input stuff

--- source/Player.hx
+++ source/Player.hx
@@ -5,6 +5,8 @@ import flixel.FlxG;
 import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.group.FlxGroup;
+import flixel.input.gamepad.FlxGamepadInputID;
+import flixel.input.keyboard.FlxKey;
 import flixel.math.FlxMath;
 
 class Player extends FlxSprite
@@ -12,11 +14,11 @@ class Player extends FlxSprite
     static inline var USE_NEW_SETTINGS = true;
     
     static inline var TILE_SIZE = 32;
-    static inline var MAX_APEX_TIME = USE_NEW_SETTINGS ? 0.35 : 0.35;
-    static inline var MIN_JUMP  = TILE_SIZE * (USE_NEW_SETTINGS ? 1.5 : 2.5);
-    static inline var MAX_JUMP  = TILE_SIZE * (USE_NEW_SETTINGS ? 3.5 : 4.5);
-    static inline var AIR_JUMP  = TILE_SIZE * (USE_NEW_SETTINGS ? 2.0 : 2.0);
-    static inline var FALL_JUMP = TILE_SIZE * (USE_NEW_SETTINGS ? 3.0 : 4.0);
+    public static inline var MAX_APEX_TIME = USE_NEW_SETTINGS ? 0.35 : 0.35;
+    public static inline var MIN_JUMP  = TILE_SIZE * (USE_NEW_SETTINGS ? 1.5 : 2.5);
+    public static inline var MAX_JUMP  = TILE_SIZE * (USE_NEW_SETTINGS ? 3.5 : 4.5);
+    public static inline var AIR_JUMP  = TILE_SIZE * (USE_NEW_SETTINGS ? 2.0 : 2.0);
+    public static inline var FALL_JUMP = TILE_SIZE * (USE_NEW_SETTINGS ? 3.0 : 4.0);
     
     static inline var MIN_APEX_TIME = 2 * MAX_APEX_TIME * MIN_JUMP / (MIN_JUMP + MAX_JUMP);
     static inline var GRAVITY = 2 * MIN_JUMP / MIN_APEX_TIME / MIN_APEX_TIME;
@@ -24,12 +26,12 @@ class Player extends FlxSprite
     static inline var JUMP_HOLD_TIME = (MAX_JUMP - MIN_JUMP) / -JUMP_SPEED;
     static var airJumpSpeed(default, never) = -Math.sqrt(2 * GRAVITY * AIR_JUMP);
     
-    inline static var JUMP_DISTANCE = TILE_SIZE * (USE_NEW_SETTINGS ? 5 : 5);
-    inline static var SLOW_DOWN_TIME       = (USE_NEW_SETTINGS ? 0.5 : 0.32);
-    inline static var GROUND_SPEED_UP_TIME = (USE_NEW_SETTINGS ? 0.15 : 0.16);
-    inline static var AIR_SPEED_UP_TIME    = (USE_NEW_SETTINGS ? 0.36 : 0.16);
-    inline static var AIRHOP_SPEED_UP_TIME = (USE_NEW_SETTINGS ? 0.5 : 0.5);
-    inline static var FALL_SPEED = (USE_NEW_SETTINGS ? -JUMP_SPEED : 520.0);
+    public inline static var JUMP_DISTANCE = TILE_SIZE * (USE_NEW_SETTINGS ? 5 : 5);
+    public inline static var SLOW_DOWN_TIME       = (USE_NEW_SETTINGS ? 0.2 : 0.135);
+    public inline static var GROUND_SPEED_UP_TIME = (USE_NEW_SETTINGS ? 0.15 : 0.16);
+    public inline static var AIR_SPEED_UP_TIME    = (USE_NEW_SETTINGS ? 0.36 : 0.16);
+    public inline static var AIRHOP_SPEED_UP_TIME = (USE_NEW_SETTINGS ? 0.5 : 0.5);
+    public inline static var FALL_SPEED = (USE_NEW_SETTINGS ? -JUMP_SPEED : 520.0);
     
     inline static var MAXSPEED = JUMP_DISTANCE / MAX_APEX_TIME / 2;
     inline static var GROUND_ACCEL = MAXSPEED / GROUND_SPEED_UP_TIME;
@@ -120,11 +122,25 @@ class Player extends FlxSprite
 
     private function movement(elapsed:Float):Void
     {
-        left = FlxG.keys.anyPressed(['LEFT', 'A']);
-        right = FlxG.keys.anyPressed(['RIGHT', 'D']);
-        jump = FlxG.keys.anyPressed(['SPACE', 'W', 'UP', 'Z', 'Y']);
-        jumpP = FlxG.keys.anyJustPressed(['SPACE', "W", 'UP', 'Z', 'Y']);
-        down = FlxG.keys.anyPressed(['S', 'DOWN']);
+        final jumpKeys :Array<FlxKey> = [SPACE, W, UP, Z, Y];
+        final downKeys :Array<FlxKey> = [DOWN, S];
+        final leftKeys :Array<FlxKey> = [LEFT, A];
+        final rightKeys:Array<FlxKey> = [RIGHT, D];
+        
+        jump  = FlxG.keys.anyPressed(jumpKeys);
+        down  = FlxG.keys.anyPressed(downKeys);
+        left  = FlxG.keys.anyPressed(leftKeys);
+        right = FlxG.keys.anyPressed(rightKeys);
+        
+        var jumpR  = FlxG.keys.anyJustReleased(jumpKeys);
+        var downR  = FlxG.keys.anyJustReleased(downKeys);
+        var leftR  = FlxG.keys.anyJustReleased(leftKeys);
+        var rightR = FlxG.keys.anyJustReleased(rightKeys);
+        
+        var jumpP  = FlxG.keys.anyJustPressed(jumpKeys);
+        var downP  = FlxG.keys.anyJustPressed(downKeys);
+        var leftP  = FlxG.keys.anyJustPressed(leftKeys);
+        var rightP = FlxG.keys.anyJustPressed(rightKeys);
         
         if (velocity.y > 0)
             maxVelocity.y = FALL_SPEED;
@@ -146,74 +162,32 @@ class Player extends FlxSprite
         }
         else
             onCoyoteGround = false;
-
-        // THESE VARIABLES HAVE UNDERSCORES SIMPLY BECAUSE I COPY PASTED IT FROM CITYHOPPIN LMAOOO
-        // https://github.com/ninjamuffin99/cityhoppin/blob/master/source/player/Player4Keys.hx
-        var _upR:Bool = false;
-		var _downR:Bool = false;
-		var _leftR:Bool = false;
-		var _rightR:Bool = false;
-		
-		_upR = FlxG.keys.anyJustReleased([UP, W, SPACE, Z, Y]);
-		_downR = FlxG.keys.anyJustReleased([DOWN, S]);
-		_leftR = FlxG.keys.anyJustReleased([LEFT, A]);
-        _rightR = FlxG.keys.anyJustReleased([RIGHT, D]);
-        
-
-		var _downP:Bool = false;
-		var _leftP:Bool = false;
-		var _rightP:Bool = false;
-		
-		_downP = FlxG.keys.anyJustPressed([DOWN, S]);
-		_leftP = FlxG.keys.anyJustPressed([LEFT, A]);
-        _rightP = FlxG.keys.anyJustPressed([RIGHT, D]);
         
         var gamepad = FlxG.gamepads.lastActive;
-		if (gamepad != null)
-		{
-			if (gamepad.anyPressed(["LEFT", "DPAD_LEFT", "LEFT_STICK_DIGITAL_LEFT"]))
-			{
-				left = true;
-			}
-			
-			if (gamepad.anyPressed(["RIGHT", "DPAD_RIGHT","LEFT_STICK_DIGITAL_RIGHT"]))
-			{
-				right = true;
-			}
-
-            if (gamepad.anyPressed([A]))
-			{
-				jump = true;
-			}
-			
-			if (gamepad.anyPressed(["DOWN", "DPAD_DOWN","LEFT_STICK_DIGITAL_DOWN"]))
-			{
-				down = true;
-			}
-
-            if (gamepad.anyJustPressed(["LEFT", "DPAD_LEFT", "LEFT_STICK_DIGITAL_LEFT"]))
-			{
-				_leftP = true;
-			}
-			
-			if (gamepad.anyJustPressed(["RIGHT", "DPAD_RIGHT","LEFT_STICK_DIGITAL_RIGHT"]))
-			{
-				_rightP = true;
-			}
-
-            if (gamepad.anyJustPressed([A]))
-			{
-				jumpP = true;
-			}
-			
-			if (gamepad.anyJustPressed(["DOWN", "DPAD_DOWN","LEFT_STICK_DIGITAL_DOWN"]))
-			{
-				_downP = true;
-			}
-
-		}
+        if (gamepad != null)
+        {
+            final leftButtons :Array<FlxGamepadInputID> = [DPAD_LEFT , LEFT_STICK_DIGITAL_LEFT ];
+            final rightButtons:Array<FlxGamepadInputID> = [DPAD_RIGHT, LEFT_STICK_DIGITAL_RIGHT];
+            final jumpButtons :Array<FlxGamepadInputID> = [A];
+            final downButtons :Array<FlxGamepadInputID> = [DPAD_DOWN , LEFT_STICK_DIGITAL_DOWN ];
+            
+            left  = left  || gamepad.anyPressed(leftButtons);
+            right = right || gamepad.anyPressed(rightButtons);
+            jump  = jump  || gamepad.anyPressed(jumpButtons);
+            down  = down  || gamepad.anyPressed(downButtons);
+            
+            leftP  = leftP  || gamepad.anyJustPressed(leftButtons);
+            rightP = rightP || gamepad.anyJustPressed(rightButtons);
+            jumpP   =  jumpP  || gamepad.anyJustPressed(jumpButtons);
+            downP  = downP  || gamepad.anyJustPressed(downButtons);
+            
+            leftR  = leftR  || gamepad.anyJustReleased(leftButtons);
+            rightR = rightR || gamepad.anyJustReleased(rightButtons);
+            jumpR    = jumpR    || gamepad.anyJustReleased(jumpButtons);
+            downR  = downR  || gamepad.anyJustReleased(downButtons);
+        }
         
-        if (isTouching(FlxObject.CEILING) || _upR)
+        if (isTouching(FlxObject.CEILING) || jumpR)
             apexReached = true;
         
         if (left != right)
