d778a3b19033c7186e380d0b4e2c050dfa6d5354
George FunBook <gkurelic@gmail.com>
2022-01-23 10:25:01 -0600

    nerf springs

--- source/props/Spring.hx
+++ source/props/Spring.hx
@@ -6,10 +6,10 @@ import zero.utilities.OgmoUtils;
 
 class Spring extends FlxSprite implements Bouncer
 {
-    public var bumpMin(default, null) = 2.0;
-    public var bumpMax(default, null) = 2.0;
+    public var bumpMin(default, null) = 1.0;
+    public var bumpMax(default, null) = 1.0;
     
-    public function new (x = 0.0, y = 0.0)
+    public function new (x = 0.0, y = 0.0, rotation:Float)
     {
         super(x, y);
         loadGraphic("assets/images/spring.png", true, 32, 32);
@@ -17,10 +17,19 @@ class Spring extends FlxSprite implements Bouncer
         offset.y = 19;
         width -= offset.x * 2;
         height -= offset.y;
+        
+        switch(rotation)
+        {
+            case 0: 
+                bumpMin = 1.0;
+                bumpMax = 1.0;
+            default:
+                throw "invalid angle";
+        }
     }
     
     static public function fromOgmo(data:EntityData)
     {
-        return new Spring(data.x + 2, data.y + 19);
+        return new Spring(data.x + 2, data.y + 19, data.rotation);
     }
 }
